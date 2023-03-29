ec666d98bbed9860719f1493a68ff5590abb4e87
Keegan Owsley <keegano@gmail.com>
2023-03-28 22:59:50 -0700

    Updates to nvim LSP settings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,10 +5,15 @@ lsp.preset({})
 lsp.ensure_installed({
   'tsserver',
   'rust_analyzer',
-  'lua_ls'
+  'lua_ls',
+  'bashls',
+  'clangd',
+  'jedi_language_server'
 })
 
-require'lspconfig'.lua_ls.setup {
+local lspconf = require('lspconfig')
+
+lspconf.lua_ls.setup {
   settings = {
     Lua = {
       runtime = {
@@ -31,25 +36,56 @@ require'lspconfig'.lua_ls.setup {
   },
 }
 
+lspconf.jedi_language_server.setup{}
+lspconf.bashls.setup{}
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm(cmp_select),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+-- lsp.on_attach(function(client, bufnr)
+--   lsp.default_keymaps({buffer = bufnr})
+-- end)
+
+-- lsp.nvim_workspace()
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>wa", function() vim.lsp.buf.add_workspace_folder() end, opts)
+  vim.keymap.set("n", "<leader>wr", function() vim.lsp.buf.remove_workspace_folder() end, opts)
+  vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp.nvim_workspace()
--- lsp.on_attach(function(client, bufnr)
---   local opts = {buffer = bufnr, remap = false}
--- 
---   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
---   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
---   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
--- end)
+lspconf.csharp_ls.setup({
+    root_dir = function(filename, _)
+        local root
+        root = lspconf.util.find_git_ancestor(filename)
+        root = root or lspconf.util.root_pattern("*.sln")(filename)
+        root = root or lspconf.util.root_pattern("*.csproj")(filename)
+        return root
+    end,
+})
 
 lsp.setup()
 
